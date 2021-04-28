7d154a59710315829da2bb241e5b5b0167fc2d3d
Aziz <aziz981@live.com>
2021-04-28 16:44:52 +0100

    Implementation Game

--- Game.py
+++ Game.py
@@ -1,34 +1,135 @@
-import numpy as np
 class Game:
- 
-    def __init(self):
-        self.Turn=1             # turn=1 >>> joueur blanc turn=2 >>>> joueur noir
-        self.winner=None    # 0 si null
+
+    def __init__(self):
+        self.turn=1
+        self.winner=None
         self.state=None
-    ''' abstract class'''
+
+    
+   
+
+
+
  
     def possibleMoves(self):
-        moves=np.array([])
-        test=np.array([[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]])
-        moves=["0,0","0,1","0,6","1,1","1,2","2,1"]
-        return moves
+
+        self.state=['1','2','3','4','5','6','7','8','9']
+        return self.state        
+
+    
+    def display_state(self):
+        print(board[0]+" | "+board[1]+" | "+board[2])
+        print(board[3]+" | "+board[4]+" | "+board[5])
+        print(board[6]+" | "+board[7]+" | "+board[8])
+
+
+
+
+    
+    def change_player(self):
+        
+        if self.turn==1:
+            self.turn=2
+        elif self.turn==2:
+            self.turn=1
+
+
 
 
     def play(self):
- 
-        '''
+        
+        self.possibleMoves()
+        self.display_state()
 
-        fonction qui fais le jeu
-                invoque la fonction possibleMoves() pour jouer
- 
-                '''
- 
-    def HasWon(self)->int:
- 
-        '''
-        fonction donnant le resultat d'une partie 
-        retourne le numéro du joueur ayant gagné  (1 ou 2)
-        si partie nulle retourne 0
- 
-        '''
+        while self.winner is None:
+            position=input(" Choissisez une position entre (1 et 9): ")
+
+            while position not in ['1','2','3','4','5','6','7','8','9']:
+                position=input("Position incorrecte, Choissisez une position entre (1 et 9): ")
+            
+
+            index=self.state.index(str(position))
+            self.state.pop(index)
+
+            position=int(position)-1
+            if board[position] != " ":
+                raise RuntimeError("Mouvement incorrecte")
+
+            if self.turn==1:
+                board[position]="X"
+            elif self.turn==2:
+                board[position]="O"
+            
+            self.display_state()
+            print("Les possibles moves restants :" + str(self.state))
+            self.HasWon()
+            
+            if self.winner is None:
+                self.change_player()
+            elif self.winner is not None:
+                if self.winner == 0:
+                    print("Match nul "+str(self.winner))
+                else:
+                    print("Joueur "+str(self.winner)+" a gagné")
+                        
     
+    def HasWon(self):
+        
+        #rows
+        if board[0]==board[1]==board[2]!=" ":
+            if board[0]=="X":
+                self.winner=1
+            elif board[0]=="O":
+                self.winner=2
+        if board[3]==board[4]==board[5]!=" ":
+            if board[3]=="X":
+                self.winner=1
+            elif board[3]=="O":
+                self.winner=2
+        if board[6]==board[7]==board[8]!=" ":
+            if board[6]=="X":
+                self.winner=1
+            elif board[6]=="O":
+                self.winner=2
+
+        #columns
+        if board[0]==board[3]==board[6]!=" ":
+            if board[0]=="X":
+                self.winner=1
+            elif board[0]=="O":
+                self.winner=2
+        if board[1]==board[4]==board[7]!=" ":
+            if board[1]=="X":
+                self.winner=1
+            elif board[1]=="O":
+                self.winner=2
+        if board[2]==board[5]==board[8]!=" ":
+            if board[2]=="X":
+                self.winner=1
+            elif board[2]=="O":
+                self.winner=2
+
+        #diagonals
+        if board[0]==board[4]==board[8]!=" ":
+            if board[0]=="X":
+                self.winner=1
+            elif board[0]=="O":
+                self.winner=2
+        if board[2]==board[4]==board[6]!=" ":
+            if board[2]=="X":
+                self.winner=1
+            elif board[2]=="O":
+                self.winner=2
+        
+        if " " not in board:
+            self.winner=0
+
+        
+
+board=[" "," "," ",
+       " "," "," ",
+       " "," "," "]
+
+
+test=Game()
+test.play()
