78c6d906e54df4d7cf232cc81fdd37bbb18a7b7a
djawed <bekhouchadjawed@gmail.com>
2021-06-23 16:42:50 +0100

    -chess_Gui.py updated successfully
    ** have to be tested

--- Main.py
+++ Main.py
@@ -11,47 +11,7 @@ class main:
 
 
 
-
-    '''def testerTicTacToe(self,game:Game):
-        game.play1(2,4)
-        print(game.state)
-    '''
-
-    #ticTac: Objet jeux, contient les méthodes "nextMoves"  # possible moves
-
     '''def play(self, game, database):
-
-                #declarations
-
-        tictac = TicTacToe()
-        currentGameState = {'board': [" ", " ", " ", " ", " ", " ", " ", " ", " "], 'nextPlayer' : "X", 'value': None}
-        player1 = Tplayer()
-        mcts = Mcts(tictac, 1)
-        mcts.CurrentGameNode = mcts.initialize(tictac, currentGameState, database)       # créer la racine et les fils de la racine
-
-
-        lastMCTSState = currentGameState
-
-        while tictac.winner== None:
-            # jeux joueur 1
-
-          currentGameState = player1.Player1Move(game, currentGameState)            # hadi elle bouge
-
-          tictac.HasWon(currentGameState['board'])
-
-          if(tictac.winner==None):
-              # jeux ordinateur
-              currentGameState = mcts.ComputerPlay(game, currentGameState, mcts.CurrentGameNode)
-              lastMCTSState = deepcopy(currentGameState)
-              tictac.HasWon(currentGameState['board'])
-
-        print("the winner is the player", tictac.winner)
-        data = connection("tictactoe")
-        deleteTree(data)
-        updateTreesearch(data, mcts.root)
-'''
-
-    def play(self, game, database):
 
                 #declarations
 
@@ -115,7 +75,7 @@ class main:
         data = connection("chess")
         deleteTree(data)
         updateTreesearch(data, mcts.root)
-
+'''
 
 
 
@@ -159,9 +119,6 @@ class main:
 
 
 
-        '''data = connection("tictactoe")
-        deleteTree(data)
-        updateTreesearch(data, mcts.root)'''
 
 
 
